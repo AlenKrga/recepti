9871d1c5e04dea50497a2efcc1f1b830ede04509
AlenKrga <alen.krga@gmail.com>
2020-05-25 11:04:17 +0000

    changed numbers to IP and PORT

--- app.py
+++ app.py
@@ -17,6 +17,6 @@ def get_tasks():
 
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('0.0.0.0'),
-            port=int(os.environ.get('5000')),
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
             debug=True)
